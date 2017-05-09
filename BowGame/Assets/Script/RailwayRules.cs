681686c8c7693d3b33dbc8a93fb6be321e326c6f
kaleidosgu <gjcaoyi@163.com>
2017-05-09 14:00:22 +0800

    新平台物件添加

---
+++ BowGame/Assets/Script/RailwayRules.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailwayRules : MonoBehaviour {
+
+    public Vector3 vecChangeDir;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if( collision.tag == "RailwayObject" )
+        {
+            FlyingPlatform flyObj = collision.GetComponent<FlyingPlatform>();
+            flyObj.ChangeDirection(vecChangeDir);
+        }
+    }
+}
